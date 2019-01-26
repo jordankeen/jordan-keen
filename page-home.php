f150e1408b4c9ceff900501e8d1b3b47f0a58db3
Jordan Keen <jordankeen.7@gmail.com>
2019-01-26 11:40:46 -0500

    initial commit

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Template Name: Home Page
+ *
+ */
+
+get_header();  
+
+?>
+
+	<main>
+		<h1>Hello. My name is Jordan Keen.I am a Front-End  Developer based in Toronto, ON. One thing I love about coding... more often than not, there is more than one way to solve a problem.</h1>
+	</main>
+	
+
+<?php get_footer(); ?>
