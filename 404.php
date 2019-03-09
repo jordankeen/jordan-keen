9c63cd743fa5749b2a55b50910b005c003e1bb06
Jordan Keen <jordankeen.7@gmail.com>
2019-03-09 09:48:29 -0500

    404 page styling

--- 404.php
+++ 404.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 
-<div class="main">
-  <div class="container">
+<div class="not-found-main">
+  <div class="not-found-container">
 
     <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
+    <p>Apologies, but the page you requested could not be found.</p>
+    <p><a href="http://www.jordankeen.com">Click Here</a> for JORDANKEEN.COM</p>
+
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
